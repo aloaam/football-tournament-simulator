42793d009d0c731e9f9396969c1dd6c4287be7d8
= <=>
2022-12-16 23:21:21 +0100

    Add skeleton of the CSV exporter

---
+++ src/main/java/com/example/footballtournamentsimulator/exporter/CsvFileExporter.java
@@ -0,0 +1,61 @@
+package com.example.footballtournamentsimulator.exporter;
+
+import com.example.footballtournamentsimulator.team.Team;
+import com.example.footballtournamentsimulator.team.TeamService;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+
+//TODO: Alo - finish this as the example, create a class object, which will hold the value and the format.
+public class CsvFileExporter {
+
+    private final TeamService teamService;
+
+    public CsvFileExporter(TeamService teamService) {
+        this.teamService = teamService;
+    }
+
+    public File export() throws IOException {
+
+
+        final File report = File.createTempFile("group_report", ".csv");
+        final FileWriter writer = new FileWriter(report);
+
+
+        final List<Team> teams = teamService.getGroupDataOrderedByPoints();
+        final CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader(
+                Headers.GROUP.name(),
+                Headers.TEAM.name(),
+                Headers.POINTS.name());
+        try (final CSVPrinter printer = new CSVPrinter(writer, csvFormat)) {
+            teams.forEach(
+                    team -> {
+                        try {
+                            printer.printRecord(
+                                    team.getTournamentGroupId().getGroupName(),
+                                    team.getTeamName(),
+                                    team.getPoints());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+            );
+            System.out.println(report.getAbsolutePath());
+        }
+
+        return null;
+    }
+
+    private enum Headers {
+        GROUP,
+        TEAM,
+        POINTS
+    }
+}
+
+
---
+++ src/main/java/com/example/footballtournamentsimulator/exporter/Value.java
@@ -0,0 +1,8 @@
+package com.example.footballtournamentsimulator.exporter;
+
+
+//TODO: Alo - this object will hold the object with a value and its format representation.
+public class Value {
+
+
+}
