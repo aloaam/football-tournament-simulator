da32465dbb2b42678be35879178278b53de8c769
= <=>
2022-12-21 22:45:10 +0100

    Add Team function to team controller to get all groups, with current points.

--- src/main/java/com/example/footballtournamentsimulator/actualdata/team/TeamController.java
+++ src/main/java/com/example/footballtournamentsimulator/actualdata/team/TeamController.java
@@ -1,12 +1,27 @@
 package com.example.footballtournamentsimulator.actualdata.team;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/teams")
 @Slf4j
 public class TeamController {
+
+
+    private final TeamService service;
+
+    public TeamController(TeamService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Team> getTeamsInGroups() {
+        return service.getTeamsGroupedByTournamentGroup();
+    }
 
 }
