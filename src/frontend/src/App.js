269194bb19fc497605efc50163a7e8296531afe1
= <=>
2023-01-01 19:12:13 +0100

    Create docker image

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,107 +1,134 @@
-import {getTeamsInGroups} from "./client";
-import {useEffect, useState} from 'react'
-import {Breadcrumb, Layout, Menu, Table} from 'antd';
-import {DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined,} from '@ant-design/icons';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons";
+import { Breadcrumb, Layout, Menu, Table, Spin, Empty } from "antd";
+
+import { useEffect, useState } from "react";
 import "./App.css";
+import { getTeamsInGroups } from "./client";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
 function App() {
-
   const [teams, setTeams] = useState([]);
-  const [collapsed, setCollapsed] = useState(false)
+  const [collapsed, setCollapsed] = useState(false);
+  const [fetching, setFetching] = useState(true);
 
-  const fetchTeams = () => 
-  getTeamsInGroups()
-    .then((res) => res.json())
-    .then(data => {
-      // console.log(JSON.stringify(data ))
-      console.log(teamSubSet)
-      setTeams(data);
-    } 
-  );
+  const fetchTeams = () =>
+    getTeamsInGroups()
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(JSON.stringify(data ))
+        console.log(teamSubSet);
+        setTeams(data);
+        setFetching(false);
+      });
 
   useEffect(() => {
     console.log("Component is  mounted");
     fetchTeams();
-  }, [])
-
+  }, []);
 
   const columns = [
     {
       title: "Group",
       dataIndex: "group",
-      key: "group"
+      key: "group",
     },
     {
       title: "Team",
       dataIndex: "teamName",
-      key: "teamName"
+      key: "teamName",
     },
     {
       title: "Points",
       dataIndex: "points",
-      key: "points"
-    }
-  ]
+      key: "points",
+    },
+  ];
 
-  const teamSubSet = teams.map(({ tournamentGroupId, teamName, points }) => {
-    var group = tournamentGroupId["groupName"]
-  return ({group,teamName, points})
-  })
+  const teamSubSet = teams.map(
+    ({ tournamentGroupId, teamName, points, id }) => {
+      var group = tournamentGroupId["groupName"];
+      return { group, teamName, points, id };
+    }
+  );
 
   const renderTeams = () => {
-    
-    console.log(teamSubSet)
-    if (teams.lentgh <= 0 ) {
-      return "No data available."
+    if (fetching) {
+      <Spin indicator={antIcon}></Spin>;
     }
-    return <Table
+
+    if (teams.lentgh <= 0) {
+      return <Empty />;
+    }
+
+    return (
+      <Table
         dataSource={teamSubSet}
-        columns={columns}/>
-  }
-  
-  return <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed}
-               onCollapse={setCollapsed}>
-            <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                <Menu.Item key="1" icon={<PieChartOutlined />}>
-                    Option 1
-                </Menu.Item>
-                <Menu.Item key="2" icon={<DesktopOutlined />}>
-                    Option 2
-                </Menu.Item>
-                <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-                    <Menu.Item key="3">Tom</Menu.Item>
-                    <Menu.Item key="4">Bill</Menu.Item>
-                    <Menu.Item key="5">Alex</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-                    <Menu.Item key="6">Team 1</Menu.Item>
-                    <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item key="9" icon={<FileOutlined />}>
-                    Files
-                </Menu.Item>
-            </Menu>
-        </Sider>
-        <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }} />
-            <Content style={{ margin: '0 16px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>User</Breadcrumb.Item>
-                    <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                </Breadcrumb>
-                <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                    {renderTeams()}
-                    {/* "Data" */}
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>Ant Design © {new Date().getFullYear()} Created by Ant UED</Footer>
-        </Layout>
+        columns={columns}
+        bordered
+        title={() => "Header"}
+        pagination={{ pageSize: 4 }}
+        rowKey={(teamSubSet) => teamSubSet.id}
+      />
+    );
+  };
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            {renderTeams()}
+            {/* "Data" */}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          By αλο © {new Date().getFullYear()}
+        </Footer>
+      </Layout>
     </Layout>
+  );
 }
 
 export default App;
